a5799a1a8452aa10113eff2d3a4ef53e2db06e8d
elena <s234571@dtu.dk>
2025-10-31 08:51:17 +0100

    defining UI components in .java logic

--- ui/app/src/main/java/com/example/comp3330_hkunite/AddFragment.java
+++ ui/app/src/main/java/com/example/comp3330_hkunite/AddFragment.java
@@ -7,58 +7,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AddFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //initializing the variables:
+    private Button addButton;
+    private Button uploadImageButton;
+    private EditText title;
+    private EditText location;
+    private EditText date;
+    private EditText time;
+    private EditText description;
+    private Switch switchPrivate;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public AddFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddFragment newInstance(String param1, String param2) {
-        AddFragment fragment = new AddFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        // here i should now create the database i think
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+
+        //connecting all the buttons to their UI button, use: findViewById(R.id.btn_add)
+        addButton = view.findViewById(R.id.addButton);
+        uploadImageButton = view.findViewById(R.id.add_image);
+        title = view.findViewById(R.id.editTextTitle);
+        location = view.findViewById(R.id.editTextLocation);
+        date= view.findViewById(R.id.editTextDate);
+        time = view.findViewById(R.id.editTextTime);
+        description = view.findViewById(R.id.editTextDescription);
+        switchPrivate = view.findViewById(R.id.switchPrivate);
+
+
+
         return inflater.inflate(R.layout.fragment_add, container, false);
     }
 }
